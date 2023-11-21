510e95ddf306e6fee5ea333fd688cadeaa1faede
Mattias Kvist <makv@kth.se>
2023-11-21 20:51:04 +0100

    Partly clean up main function

--- main.c
+++ main.c
@@ -58,57 +58,54 @@ int main(void)
 		/* SPI2CON bit ON = 1; */
 		SPI2CONSET = 0x8000;
 
+		/* Initialize display */
+		display_init();
+
 		/* Set up timer */
-		// T2CON = 0x70; // Stop timer and set prescaling to 1:256
-		// TMR2 = 0;					 // Reset counter
-		// PR2 = (80000000 / 256) / 10; // Set period to 100ms
+		TMR2 = 0;					 // Reset timer counter
+		PR2 = (80000000 / 256) / 10; // Set period to 100ms
+		T2CON = 0x8070;     // Enable timer 2 (bit 15) and set prescaler to 1:256 (bit 6 and 7)
 
 		// /* Set up interrupts for timer*/
-		// IPCSET(2) = 0x1F;	  // Set priority 7 and subpriority 3
-		// IFSCLR(0) = (1 << 8); // Clear interrupt flag for timer 2
-		// IECSET(0) = (1 << 8); // Enable interrupts for timer 2
+		IPCSET(2) = 0x1F;	  // Set priority 7 and subpriority 3
+		IFSCLR(0) = (1 << 8); // Clear interrupt flag for timer 2
+		IECSET(0) = (1 << 8); // Enable interrupts for timer 2
 
 		/* Set up interrupts for switches */
-		// IECSET(0) = 0x80000;	// Enable switch 4 interrupts (bit 19)
-		// IECSET(0) = 0x8000;		// Enable switch 3 interrupts (bit 15)
-		// IECSET(0) = 0x800;		// Enable switch 2 interrupts (bit 11)
-		// IECSET(0) = 0x80;		// Enable switch 1 interrupts (bit 7)
+		IECSET(0) = 0x80000;	// Enable switch 4 interrupts (bit 19)
+		IECSET(0) = 0x8000;		// Enable switch 3 interrupts (bit 15)
+		IECSET(0) = 0x800;		// Enable switch 2 interrupts (bit 11)
+		IECSET(0) = 0x80;		// Enable switch 1 interrupts (bit 7)
 
 
+		/* Set up change notice interrupts for button 1 */
+		TRISD = TRISD | 0x80;
+		CNCON = 0x8000;
+		IECSET(1) = 0x1;
+		IPCSET(4) = 0x1c000000;
+		IPCSET(6) = 0x1C0000;
+		CNEN = 0x10000;
 
-		// TRISD = TRISD | 0x80;
-		// CNCON = 0x8000;
-
-		// IECSET(1) = 0x1;
-		// IPCSET(4) = 0x1c000000;
-		// IPCSET(6) = 0x1C0000;
-		// CNEN = 0x10000;
-
-		// /* Set up input pins*/
-		// TRISDSET = (1 << 8);
-		// TRISFSET = (1 << 1);
+		/* Set up input pins*/
+		TRISDSET = (1 << 8);
+		TRISFSET = (1 << 1);
 
 		// IECSET(0) = 0x100;		// Enable T2 interrupts (bit 8)
-		// IECSET(0) = 0x80000;	// Enable switch 4 interrupts (bit 19)
-		// IECSET(0) = 0x8000;		// Enable switch 3 interrupts (bit 15)
-		// IECSET(0) = 0x800;		// Enable switch 2 interrupts (bit 11)
-		// IECSET(0) = 0x80;	// Enable switch 1 interrupts (bit 7)
 		// IPCSET(2) = 0b11100;	// Set priority level 7
 		// IPCSET(4) = 0x1c000000; // Set priority level 7 // Bits 28-26
-		 // Initialize port D so that bits 11 through 5 of Port D are set as inputs
+		// //Initialize port D so that bits 11 through 5 of Port D are set as inputs
 	  	// TRISD = TRISD | 0x0fe0;
 
 		/* Initialize I2C*/
-		// I2C1CON = 0x0; // Clear control register
-		// I2C1BRG = 0x0C2; // Set Baud Generator Divisor to 100
-		// I2C1STAT = 0x0; // Clear status register
-		// I2C1CONSET = 1 << 13; // SIDL = 1 (disables I2C when CPU is idle)
-		// I2C1ADD = 0b1010000; // add EEPROM address to I2C1ADD register
+		I2C1CON = 0x0; // Clear control register
+		I2C1BRG = 0x0C2; // Set Baud Generator Divisor to 100
+		I2C1STAT = 0x0; // Clear status register
+		I2C1CONSET = 1 << 13; // SIDL = 1 (disables I2C when CPU is idle)
+		I2C1CONSET = 1 << 15; // I2C ON
+		// I2C1ADD = 0b1010000; // add EEPROM address to I2C1ADD register...THIS MESSED THINGS UP
 
-		// enable_interrupt();		// Enable global interrupts
+		enable_interrupt();		// Enable global interrupts
 
-		// T2CONSET = 0x8000;		// Start clock
-		// I2C1CON = 1 << 15; // I2C ON
  //----------------------------------------------
 
 	// /* Set up peripheral bus clock */
@@ -132,12 +129,12 @@ int main(void)
 	// TRISECLR = 0xFF;
 
 	/* Set up input pins */
-	TRISFSET = 0x2;	  // Button 1
-	TRISDSET = 0xE0;  // Buttons 2-4
-	TRISDSET = 0xF00; // Switches 1-4
+	// TRISFSET = 0x2;	  // Button 1
+	// TRISDSET = 0xE0;  // Buttons 2-4
+	// TRISDSET = 0xF00; // Switches 1-4
 
-	/* Set up output pins */
-	TRISECLR = 0xFF;
+	// /* Set up output pins */
+	// TRISECLR = 0xFF;
 
 	// /* Set up SPI as master */
 	// SPI2CON = 0;
@@ -151,31 +148,29 @@ int main(void)
 	// /* SPI2CON bit ON = 1; */
 	// SPI2CONSET = 0x8000;
 
-	display_init();
-
 	/* Set up clock */
 	/* Initialize clock */
-	T2CON = 0x70; // Stop timer and set prescaling to 1:256
-	TMR2 = 0;					 // Reset counter
-	PR2 = (80000000 / 256) / 10; // Set period to 100ms
+	// T2CON = 0x70; // Stop timer and set prescaling to 1:256
+	// TMR2 = 0;					 // Reset counter
+	// PR2 = (80000000 / 256) / 10; // Set period to 100ms
 
-	IPCSET(2) = 0x1F;	  // Set priority 7 and subpriority 3
-	IFSCLR(0) = (1 << 8); // Clear interrupt flag for timer 2
-	IECSET(0) = (1 << 8); // Enable interrupts for timer 2
+	// IPCSET(2) = 0x1F;	  // Set priority 7 and subpriority 3
+	// IFSCLR(0) = (1 << 8); // Clear interrupt flag for timer 2
+	// IECSET(0) = (1 << 8); // Enable interrupts for timer 2
 
 	// /* Initialize I2C */
-	I2C1CON = 0x0;		  // Clear control register
-	I2C1BRG = 0x0C2;	  // Set Baud Generator Divisor
-	I2C1STAT = 0x0;		  // Clear status register
-	I2C1CONSET = 1 << 13; // SIDL = 1
+	// I2C1CON = 0x0;		  // Clear control register
+	// I2C1BRG = 0x0C2;	  // Set Baud Generator Divisor
+	// I2C1STAT = 0x0;		  // Clear status register
+	// I2C1CONSET = 1 << 13; // SIDL = 1
 	// // IPCSET(6) = 0x1B00;       // Set priority 6 and subpriority 3
 	// // IFSCLR(0) = 0xE0000000;   // Clear interrupt flags for I2C1MIF, I2C1SIF and I2CBIF
 	// // IECSET(0) = 0xE0000000;   // Enable interrupts for I2C1MIF, I2C1SIF and I2CBIF
 
-	enable_interrupt(); // Enable interrupts globally
+	// enable_interrupt(); // Enable interrupts globally
 
-	T2CONSET = 0x8000;	 // Start clock
-	I2C1CONSET = 0x8000; // Start I2C bus
+	// T2CONSET = 0x8000;	 // Start clock
+	// I2C1CONSET = 0x8000; // Start I2C bus
 
 	// display_init();
 	display_string(0, "Score: ");

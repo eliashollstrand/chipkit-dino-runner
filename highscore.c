533d61273c67fea9223540de4c36c4ab8610c7f9
Mattias Kvist <makv@kth.se>
2023-11-21 21:41:30 +0100

    Add clean and working I2C

--- highscore.c
+++ highscore.c
@@ -18,12 +18,14 @@ uint8_t highscore;
 uint8_t score;
 int ledValue = 0;
 
+/* Initialize LEDs*/
 volatile int *porte = (volatile int *)0xbf886110;
+volatile int *trise = (volatile int *)0xbf886100;
 
 #define EEPROM_WRITE 0xA0 // 1010 0000
 #define EEPROM_READ 0xA1  // 1010 0001
 #define SCORE_ADDRESS 0x0230
-
+#define I2C_DELAY 500000 // Delay to allow the EEPROM to complete the write operation, experiment more
 
 // uint8_t current_address_read() {
 //     uint8_t data = 0;
@@ -68,6 +70,7 @@ uint8_t eeprom_read_byte(uint16_t address) {
     return data;
 }
 
+
 void eeprom_write_byte(uint16_t address, uint8_t data) {
     // Step 1: Send start condition
     i2c_start();
@@ -86,9 +89,10 @@ void eeprom_write_byte(uint16_t address, uint8_t data) {
     i2c_stop();
 
     // Add a delay to allow the EEPROM to complete the write operation
-
+    delay(I2C_DELAY); // Delay to allow the EEPROM to complete the write operation, can display 255 without delay if read too quickly
 }
 
+/* Function used to delay operations */
 void delay(int cyc)
 {
     int i;
@@ -98,9 +102,9 @@ void delay(int cyc)
 
 void score_display()
 {
+    display_string(0, "Score: ");
     display_string(1, itoaconv(score));
 
-    // highscore = eeprom_read_byte(SCORE_ADDRESS);
     display_string(3, itoaconv(highscore));
     display_update();
 }
@@ -108,34 +112,28 @@ void score_display()
 void score_update()
 {
     score++;
-    if ((int)score > highscore)
+    if (score > highscore)
     {
-        highscore = score;
-        eeprom_write_byte(SCORE_ADDRESS, highscore);
-    }
+        // highscore = score;
+        eeprom_write_byte(SCORE_ADDRESS, score);
 
+    }
     score_display();
 }
 
 /* Interrupt Service Routine */
 void user_isr(void)
 {
-    if (IFS(1) & 0x1)
-    { // button 4
-        // read_highscore();
+    if (IFS(1) & 0x1) // button 4, update score
+    {
         score_update();
-        delay(1000000);
         IFSCLR(1) = 0x1;
     }
 
-    if (IFS(0) & 0x80000) // switch 4, update led+=1
+    if (IFS(0) & 0x80000) // switch 4, update led+=1 and write ledvalue to EEPROM
     {
         IFSCLR(0) = 0x80000;
-        // Increase the LED value by 1 (roll over after 255)
-        ledValue = (ledValue + 1) & 0xFF;
-
-        // Update the LEDs
-        *porte = ledValue;
+        increment_LEDs();
 
         eeprom_write_byte(SCORE_ADDRESS, ledValue);
         display_string(0, "Wrote to EEPROM");
@@ -143,14 +141,9 @@ void user_isr(void)
         display_update();
     }
 
-    if (IFS(0) & 0x8000) // switch 3, update led and read from EEPROM
+    if (IFS(0) & 0x8000) // switch 3, read from EEPROM
     {
         IFSCLR(0) = 0x8000;
-        // Increase the LED value by 1 (roll over after 255)
-        // ledValue = (ledValue + 1) & 0xFF;
-
-        // // Update the LEDs
-        // *porte = ledValue;
 
         highscore = eeprom_read_byte(SCORE_ADDRESS);
         display_string(0, "Read from EEPROM");
@@ -158,44 +151,38 @@ void user_isr(void)
         display_update();
     }
 
-    if (IFS(0) & 0x800) // switch 2, update led and read from EEPROM
+    if (IFS(0) & 0x800) // switch 2
     {
         IFSCLR(0) = 0x800;
-        // Increase the LED value by 1 (roll over after 255)
-        ledValue = (ledValue + 1) & 0xFF;
-
-        // Update the LEDs
-        *porte = ledValue;
-
-        // highscore = readFromEEPROM(SCORE_ADDRESS);
-        // read_highscore();
-        // display_string(0, "Read from EEPROM");
-        display_string(2, display_update());
-        display_update();
+        increment_LEDs();
     }
+
 
     if (IFS(0) & 0x80) // switch 1, update led and update display
     {
         IFSCLR(0) = 0x80;
-        // Increase the LED value by 1 (roll over after 255)
-        ledValue = (ledValue + 1) & 0xFF;
-
-        *porte = ledValue;
-
-        display_update();
+        increment_LEDs();
     }
 }
 
 void score_init()
 {
-    // Enable global interrupts
-    enable_interrupt();
 
-    volatile int *trise = (volatile int *)0xbf886100;
+    // Display string to indicate that the highscore is stored in EEPROM
+	display_string(2, "Score in EEPROM: ");
 
     // Get the high score from EEPROM and store in highscore variable
     highscore = eeprom_read_byte(SCORE_ADDRESS);
 
     // Display score and current highscore
     score_display();
+}
+
+void increment_LEDs()
+{
+    // Increase the LED value by 1 (roll over after 255)
+    ledValue = (ledValue + 1) & 0xFF;
+
+    // Update the LEDs
+    *porte = ledValue;
 }
